c319301e4ee759c7f5eb51dfb63f4cbd13703280
Bruno Zenji <brunozenji@gmail.com>
2019-07-01 03:19:10 -0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Dotenv\Validator;
+use Illuminate\Http\Request;
+use App\Receita;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,10 +16,63 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/receitas', function () {
+    $id_user = Auth::id();
+
+    $receitas = Receita::orderBy('created_at', 'asc')->where('id_user', $id_user)->get();
+
+    return view('receitas.receita', [
+        'receitas' => $receitas
+    ]);
+})->middleware("auth");
+
+
+Route::post('/receita', function (Request $request) {
+
+    $rules = [
+        'title' => 'required|max:255',
+        'ingredient' => 'required',
+        'preparation' => 'required'
+    ];
+
+    $messages = [
+        'title.required' => 'É obrigatório inserir um título na receita',
+        'preparation.required' => 'É obrigatório inserir um modo de preparo na receita',
+        'ingredient.required' => 'É obrigatório inserir ingredientes na receita',
+    ];
+
+
+    $validator = Validator($request->all(),$rules,$messages);
+
+    if($validator->fails())
+    {
+        return redirect('/receitas')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //else create recipe
+    $id_user = Auth::id();
+    $receita = new Receita();
+    $receita->title = $request->title;
+    $receita->preparation = $request->preparation;
+    $receita->ingredient = $request->ingredient;
+    $receita->id_user = $id_user;
+
+    $receita->save();
+
+    return redirect('/receitas');
+});
+
+Route::delete('/receita/{receita}', function (Receita $receita) {
+    $receita->delete();
+
+    return redirect('/receitas');
+});
